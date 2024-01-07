9e26e1b019eff84e85601add9abf958aa0433bd8
Inun572 <helmi.ainun@gmail.com>
2024-01-07 12:12:14 +0700

    add scroll to top button

--- src/components/layouts/BackToTopButton.jsx
+++ src/components/layouts/BackToTopButton.jsx
@@ -1,5 +1,20 @@
-const BackToTopButton = () => {
-  return <div>BackToTopButton</div>;
+import PropTypes from 'prop-types';
+
+const BackToTopButton = ({ onClick }) => {
+  return (
+    <div className="fixed bottom-6 right-4">
+      <button
+        onClick={onClick}
+        className="w-[60px] h-[60px] rounded-full flex justify-center items-center bg-accent text-xl font-semibold transition-all duration-300 hover:-translate-y-1 hover:shadow-lg hover:shadow-slate-400"
+      >
+        ^
+      </button>
+    </div>
+  );
+};
+
+BackToTopButton.propTypes = {
+  onClick: PropTypes.func,
 };
 
 export default BackToTopButton;
--- src/layout/NoAuth.jsx
+++ src/layout/NoAuth.jsx
@@ -3,14 +3,40 @@ import PropTypes from 'prop-types';
 import Navbar from '../components/layouts/Navbar';
 import FooterCTA from '../components/layouts/FooterCTA';
 import Footer from '../components/layouts/Footer';
+import BackToTopButton from '../components/layouts/BackToTopButton';
+import { useEffect, useState } from 'react';
 
 const NoAuth = ({ children }) => {
+  const [scroll, setScroll] = useState(0);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setScroll(window.scrollY);
+      if (scroll > 30) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  });
+
+  const onBackToTopHandle = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className="max-w-screen min-h-screen px-[100px] grid-flow-row">
-      <Navbar />
-      <div className="w-full min-h-screen">{children}</div>
+      <Navbar scrolled={show} />
+      <main className="w-full min-h-screen my-[160px] isolate">{children}</main>
       <FooterCTA />
       <Footer />
+      {show && <BackToTopButton onClick={onBackToTopHandle} />}
     </div>
   );
 };
