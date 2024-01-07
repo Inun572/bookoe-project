ca65395ba3fb002b4ca196ceb0a749b0e1d48f4f
Inun572 <helmi.ainun@gmail.com>
2024-01-07 14:22:30 +0700

    fix search books feature

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,63 +1,60 @@
-import { useEffect, useState } from 'react';
 import SmallCard from '../../components/SmallCard';
 import { useParams } from 'react-router-dom';
+import useGetData from '../../hooks/useGetData';
+
+const BASE_URL_API_BOOKS = 'https://bookapi.cm.hmw.lol/api/books';
 
 const Index = () => {
-  const [searchResult, setSearchResult] = useState([]);
-  const [status, setStatus] = useState('idle');
-  const [total, setTotal] = useState(0);
   const keyword = useParams().keyword;
 
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [keyword]);
+  const {
+    data: books,
+    error,
+    isLoading,
+  } = useGetData(`${BASE_URL_API_BOOKS}?search=${keyword}`);
 
-  const fetchData = async () => {
-    setStatus('loading');
-    const response = await fetch(
-      `https://bookapi.cm.hmw.lol/api/books?search=${keyword}`,
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      }
+  if (error) {
+    return (
+      <div className="w-full min-h-screen flex justify-center items-center">
+        <div className="text-3xl font-semibold">{error.message}</div>
+      </div>
     );
+  }
 
-    const result = await response.json();
-    setStatus('success');
-    setSearchResult(result.data);
-    setTotal(result.meta.total);
-  };
-
-  if (status === 'loading') {
+  if (isLoading) {
     return (
       <div className="w-full min-h-screen flex justify-center items-center">
         <div className="text-3xl font-semibold">Loading...</div>
       </div>
     );
   }
-  if (status === 'success' && searchResult.length < 1) {
+
+  const searchResult = books.data;
+  const total = books.meta.total;
+
+  if (searchResult?.length < 1) {
     return (
-      <div className="w-full min-h-screen flex justify-center items-center">
-        <div className="text-3xl font-semibold">Book Not Found!</div>
+      <div className="w-full min-h-[600px] flex justify-center items-center border border-accent rounded-lg">
+        <div className="text-5xl text-accent font-semibold">
+          Book Not Found!
+        </div>
       </div>
     );
   }
   return (
-    <div>
+    <>
       <div className="w-full pl-[71px] py-[42px] bg-hero rounded-lg mb-[67px]">
         <h1 className="text-[42px] font-semibold">
-          <span className="text-accent">Search For</span> {keyword}{' '}
+          <span className="text-accent">Search For</span> {keyword}
           {total && `(${total} results)`}
         </h1>
       </div>
       <div className="flex flex-wrap justify-evenly gap-[67px]">
-        {searchResult.map((book) => (
+        {searchResult?.map((book) => (
           <SmallCard key={book.id} {...book} />
         ))}
       </div>
-    </div>
+    </>
   );
 };
 
